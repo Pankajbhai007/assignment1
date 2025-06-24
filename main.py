fb9768a6781037a9fac92d0c642bbfe45969ddd0
Giri <Pankajkumar.Giri@lumen.com>
2025-06-24 18:01:43 +0530

    task1 and task2

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-num1 = int(input("Enter the first number : "))
+'''num1 = int(input("Enter the first number : "))
 num2 = int(input ("Enter the second number : "))
 print("Addition : ",num1+num2)
 print("Subtraction : ",num1-num2)
 print("Multiplication : ",num1*num2)
 print("Division : ",num1/num2)
+'''
+
+a1 = input("Enter your first name : ")
+a2 = input("Enter your last name : ")
+print("hello" ",", a1 + " " + a2 + "! welcome to the python program.")
